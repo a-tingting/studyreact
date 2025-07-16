53e9fd2b0eaa20416cf30917cc2cf3604147e127
xyt <13333916152@163.com>
2025-07-16 14:07:57 +0800

    init

--- src/pages/react04/index.tsx
+++ src/pages/react04/index.tsx
@@ -10,6 +10,9 @@ export default function index() {
             <div>react04</div>
             <div className="box1"></div>
             <div className="box2"></div>
+            <div className="box3"></div>
+            <div className="box4"></div>
+            <div className="box5"></div>
         </div>
     )
 }
